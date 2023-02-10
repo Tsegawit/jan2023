e5ecfbd86098f1c6d3407d3755f8c9ae8f19f87f
Tsegawit <tsegawitmilky@gmail.com>
2023-02-09 20:27:15 -0500

    initial commit of the project

---
+++ src/controller/calculation.java
@@ -0,0 +1,25 @@
+package controller;
+
+public class Calculation {
+    public static void main(String[] args) {
+
+            int firstNumber =10;
+            int secondNumber =8;
+            int result;
+
+            firstNumber =10;
+            secondNumber =8;
+
+            result = firstNumber + secondNumber;
+            System.out.println("add Result: "+result);
+
+            result = firstNumber - secondNumber;
+            System.out.println("subtraction Result: "+result);
+
+            result = firstNumber * secondNumber;
+            System.out.println("multiplication Result: "+result);
+
+            result = firstNumber / secondNumber;
+            System.out.println("division Result: "+result);
+        }
+    }
