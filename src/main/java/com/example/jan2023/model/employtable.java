ede9faecbe0d7e232730e0d85fc45e47ead064c1
Tsegawit <tsegawitmilky@gmail.com>
2023-02-16 20:43:50 -0500

    second commit of the project

--- src/main/java/com/example/jan2023/model/employtable.java
+++ src/main/java/com/example/jan2023/model/employtable.java
@@ -1,5 +1,19 @@
 package com.example.jan2023.model;
 public class employtable {
+    public class Employee {
+        public int employeeId;
+        public String name;
+        public String description;
+
+
+        public String toString () {
+            StringBuilder buf = new StringBuilder ();
+            buf.append ("\n\tEmployee ID = 20") .append (employeeId);
+            buf.append ("\n\tName =javi ").append (name);
+            buf.append ("\n\tDescription = labratory").append (description);
+            return buf.toString ();
+        }
+    }
 
 
 }
